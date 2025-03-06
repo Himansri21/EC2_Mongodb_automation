0ee70ed769a723b963216c15eb4c0db5feeeb3f3
Himanshu <137988128+Himansri21@users.noreply.github.com>
2025-03-06 18:58:18 +0530

    Create setup_mongodb.sh

---
+++ setup_mongodb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update -y
+
+curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo gpg --dearmor -o /usr/share/keyrings/mongodb-server-6.0.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+
+sudo apt update 
+
+sudo apt install -y mongodb-org
+
+echo "MongoDB installation and setup completed."

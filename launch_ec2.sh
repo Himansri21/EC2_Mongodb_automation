380951e778e768543714ee97f945c04cc5e8dbce
Himanshu <137988128+Himansri21@users.noreply.github.com>
2025-03-06 18:56:15 +0530

    Create launch_ec2.sh

---
+++ launch_ec2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check for required parameter (number of EC2 instances)
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <number_of_instances>"
+    exit 1
+fi
+
+NUM_INSTANCES=$1
+
+# configuration variables
+AMI_ID="ami-04b4f1a9cf54c11d0"
+INSTANCE_TYPE="t2.micro"
+KEY_NAME="NextCloudKey"
+SECURITY_GROUP="sg-0e2626e6d65233d81"
+
+echo "Launching ${NUM_INSTANCES} EC2 instance(s)..."
+
+
+aws ec2 run-instances \
+    --image-id "$AMI_ID" \
+    --count "$NUM_INSTANCES" \
+    --instance-type "$INSTANCE_TYPE" \
+    --key-name "$KEY_NAME" \
+    --security-group-ids "$SECURITY_GROUP" \
+    --user-data file://setup_mongodb.sh
+
+echo "EC2 instance(s) launched."
